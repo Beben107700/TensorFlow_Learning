ba8c2e998162df0313e820f36c92c0ef2a0d7eed
Beben107700 <ben.delcoigne@gmail.com>
2020-03-22 14:53:48 +0100

    Minor tweaks

--- fashion/fashion.py
+++ fashion/fashion.py
@@ -22,16 +22,6 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 train_images = train_images/255.0
 test_images = test_images/255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-#plt.show()
-
 
 #Creating the model
 model = keras.Sequential([
@@ -46,7 +36,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 
-model.fit(train_images, train_labels, epochs=5)
+model.fit(train_images, train_labels, epochs=10)
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
